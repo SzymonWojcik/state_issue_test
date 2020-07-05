a847b8e1cdb46c90c8bd93f04c5a9776818f0a5a
Szymon Wojcik <szymon@crustlab.com>
2020-07-05 18:28:24 +0200

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Szymon Wójcik on 05/07/2020.
+//  Copyright © 2020 Szymon Wójcik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
